512d9a1552f3aa54506640683a6892d4df2cb9be
deandamanik <deandamanik0706@gmail.com>
2026-09-15 13:57:53 +0800

    feat: initialize project setup with tailwind css, fonts, and theme colors

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '@fontsource-variable/quicksand';
+import '@fontsource/quando';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
