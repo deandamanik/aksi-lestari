6adc0740996e2f282e82086259dac6928a28e131
deandamanik <deandamanik0706@gmail.com>
2026-09-17 00:46:22 +0800

    feat: build environmental problem scroll story

--- src/pages/beranda/BerandaPage.jsx
+++ src/pages/beranda/BerandaPage.jsx
@@ -1,9 +1,13 @@
 import HeroSection from './components/HeroSection'
+import ProblemStorySection from './components/ProblemStorySection'
+import ActionStepsPreviewSection from './components/ActionStepsPreviewSection'
 
 function BerandaPage() {
   return (
     <main>
       <HeroSection />
+      <ProblemStorySection />
+      <ActionStepsPreviewSection />
     </main>
   )
 }
---
+++ src/pages/beranda/components/ActionStepsPreviewSection.jsx
@@ -0,0 +1,12 @@
+function ActionStepsPreviewSection() {
+  return (
+    <section
+      aria-hidden="true"
+      className="min-h-screen w-full bg-neutral border-t border-border-warm/30"
+    >
+      {/* Intentionally empty scroll test section to verify Section 2 unpinning & page flow */}
+    </section>
+  )
+}
+
+export default ActionStepsPreviewSection
